61ef4b233d47e0b0ba7d1b1611b64d61ad8bda7b
Danny22 <125704277+DannyGB22@users.noreply.github.com>
2024-05-08 14:57:50 -0600

    Creación de arreglos y modificación de sus valores

---
+++ intro/ULTIMATEJS/01-tipos/07-arrays.js
@@ -0,0 +1,17 @@
+
+
+let animales = ['perro', 'Caballo']
+console.log(animales) // imprmir arreglo 
+console.log(animales[0]) // imprimir el primer elemento del arreglo
+console.log(animales[1]) // imprimir el segundo elemento del arreglo
+
+animales[0] = 'Gato' // modificar el primer elemento del arreglo
+console.log(animales)
+
+animales[10] = 'dragon'
+console.log(animales[7])
+console.log(typeof animales)
+console.log(animales.length) // tamaño del arreglo
+
+
+

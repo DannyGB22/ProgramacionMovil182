0d3aa80ef1c3e18b5c3f8ebe2f9c8d072c5b6dbf
Danny22 <125704277+DannyGB22@users.noreply.github.com>
2024-05-08 09:54:02 -0600

    Creación de variables  y su diferente notación

---
+++ intro/ULTIMATEJS/01-tipos/01-vaiables.js
@@ -0,0 +1,16 @@
+
+let nombre = 'hola mundo';
+
+let NombreCompleto;
+let nombreCompleto;
+let nombre_completo;
+
+console.log(nombreCompleto);
+
+let apellido;
+let animal;
+apellido = 'Perez';
+
+
+
+

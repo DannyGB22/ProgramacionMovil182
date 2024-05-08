84537423d8f486a0a871bad90d2f75931b6f9d41
Danny22 <125704277+DannyGB22@users.noreply.github.com>
2024-05-08 14:48:27 -0600

    Creación de diferentes tipos de comentarios y creación de  objetos  con atributos.

---
+++ intro/ULTIMATEJS/01-tipos/05-comentarios.js
@@ -0,0 +1,16 @@
+
+let numero = 42; // comentario de una sola linea
+
+/*
+Todo lo que este dentro de este bloque de comentarios
+de los slash y asterisco no se va a ejecutar
+
+*/
+
+/** 
+ * 
+ * Comentario de muchas lienas
+ */
+console.log(numero);
+
+
---
+++ intro/ULTIMATEJS/01-tipos/06-objetos.js
@@ -0,0 +1,27 @@
+
+// personaje de Tv
+
+let nombre = 'Tanjiro'
+let anime = 'Demon Slayer'
+let edad = 16
+
+let personaje = {
+    nombre: 'Tanjiro',
+    anime: 'Demon Slayer',
+    edad: '16',
+};
+console.log(personaje)
+console.log(personaje.anime)
+console.log(personaje['anime'])
+
+// modificar un atributo
+
+personaje.edad = 17
+personaje['edad'] = 18
+console.log(personaje.edad)
+
+
+
+
+
+

e9fa8146f162d667f780a55f64d85bb6b24fc633
Danny22 <125704277+DannyGB22@users.noreply.github.com>
2024-05-10 15:28:42 -0600

    creación de ciclo do while

---
+++ intro/ULTIMATEJS/03-control-flujo.js/05-do-while.js
@@ -0,0 +1,18 @@
+
+let i = 0
+
+while(i < 2){
+    if (i % 2 == 0){
+        console.log('Nuemero Par', i)
+    }
+    i++
+}
+
+
+do {
+    if (i % 2 == 0){
+        console.log('Nuemero Par', i)
+    }
+    i++
+
+} while (i < 2)

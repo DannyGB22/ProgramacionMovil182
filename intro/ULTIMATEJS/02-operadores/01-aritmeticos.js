214361a1317222b961145c8a2ab567ca4f959bdb
Danny22 <125704277+DannyGB22@users.noreply.github.com>
2024-05-10 10:07:11 -0600

    ejemplos con operadores aritméticos

---
+++ intro/ULTIMATEJS/02-operadores/01-aritmeticos.js
@@ -0,0 +1,24 @@
+
+let a = 5; // declaraciones 
+let b = 7;
+
+// console.log(a + b, 'suma'); // expresiones
+// console.log(a - b, 'resta'); 
+// console.log(a * b, 'multiplicacion'); 
+// console.log(a ** b, 'potencia'); 
+// console.log(a / b, 'division'); 
+// console.log(a % b, 'modulo');
+
+
+
+
+
+// incrementar
+console.log(++a)
+console.log(a++)
+console.log(a)
+
+// decrementar
+console.log(--a)
+console.log(a--)
+console.log(a)

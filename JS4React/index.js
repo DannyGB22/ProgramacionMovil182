6e796a08b1a5a5177eb18f567845dfbd98c89e38
Danny22 <125704277+DannyGB22@users.noreply.github.com>
2024-05-20 15:41:21 -0600

    Creación de los archivos index js, css y html.  Creación de funciones Fat Arrow  y creación de objetos.

---
+++ JS4React/index.js
@@ -0,0 +1,41 @@
+console.log('Hola Mundo')
+
+
+// function suma(a, b){
+//     return a+b
+
+// }
+
+
+
+// console.log(suma(2,3));
+
+// function fatArrow
+// const suma=(a, b)=>{
+//     return a+b
+// }
+
+// console.log(suma(2,2));
+
+//
+
+// function fat Arrow simplificada
+const suma=(a, b)=> a+b
+
+
+const usuario = {
+    nombre: "Daniel",
+    Apellido: "Galvan",
+    edad: 22,
+
+    Direccion:{
+        pais: 'Mexico', ciudad:'Queretaro', calle:'La misma 123'
+    },
+
+    amigos: ['Alex', 'Carlos'],
+    estatus: true,
+    enviaCorreo: enviaCorr=()=>'enviado...'
+
+}
+
+

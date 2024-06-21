89182180dcc69b0485de0fc138d87dd412b995e4
Danny22 <125704277+DannyGB22@users.noreply.github.com>
2024-06-21 15:15:14 -0600

    Creacion de ImageBackground, para imagen de fondo y SplashScreen para imagen al cargar nuestro proyecto.

--- React-Native/App.js
+++ React-Native/App.js
@@ -1,20 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Alert, ImageBackground } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [nombre, setNombre] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const showAlert = () => {
+    alert(
+      `Nombre: ${nombre}\nEmail: ${email}\nPassword: ${password}`,
+      [{ text: "OK" }]
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={require('./fondo.jpg')} style={styles.outerContainer} resizeMode="cover">
+      <View style={styles.innerContainer}>
+        <Text style={styles.text}>Nombre:</Text>
+        <TextInput style={styles.input} onChangeText={(t) => setNombre(t)} value={nombre} />
+
+        <Text style={styles.text}>Email:</Text>
+        <TextInput style={styles.input} onChangeText={(t) => setEmail(t)} value={email} />
+
+        <Text style={styles.text}>Password:</Text>
+        <TextInput style={styles.input} onChangeText={(t) => setPassword(t)} value={password} secureTextEntry />
+
+        <View style={styles.buttonContainer}>
+          <Button color="black" title="Guardar" onPress={showAlert} />
+        </View>
+
+        <StatusBar style="auto" />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  outerContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center', // Alineado al centro horizontalmente
+    justifyContent: 'center', // Alineado al centro verticalmente
+  },
+  innerContainer: {
+    backgroundColor: '#fff', // Fondo blanco
+    padding: 20, // Padding agregado
+    borderRadius: 10, // Bordes redondeados
+    alignItems: 'center', // Alineado al centro horizontalmente
+    width: '90%', // Usar el 90% del ancho del contenedor
+  },
+  input: {
+    height: 40,
+    width: '80%', // Usar el 80% del ancho del contenedor
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    borderRadius: 5,
+  },
+  text: {
+    textAlign: 'left', // Alineación a la izquierda
+    color: 'green', 
+    width: '80%', // Ancho del texto al 80% del contenedor
+  },
+  buttonContainer: {
+    marginTop: 20,
+    width: '80%', // Ancho del contenedor del botón al 80%
   },
 });
